9b3dff256d62ad538a26d0fcf7495d758c19a7d8
bigcolbert <93502079+bigcolbert@users.noreply.github.com>
2026-08-17 14:03:22 +0800

    Delete functions/api directory

--- functions/api/inspection.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest({request}){
- const data = await request.json()
-
- return Response.json({
-   success:true,
-   message:"点检提交成功",
-   data
- })
-}
--- functions/api/wechat.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(){
- return Response.json({
-   success:true,
-   message:"企业微信接口"
- })
-}
